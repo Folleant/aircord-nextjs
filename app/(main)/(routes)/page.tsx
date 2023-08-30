601c8556e3633c8a1065b75d7224b59b5a08e119
Folleant <122909695+Folleant@users.noreply.github.com>
2023-08-31 05:11:49 +0600

    Adding a theme component, dark and light theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,13 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
     <div>
-      This is a protected route.
+      <UserButton 
+        afterSignOutUrl='/'
+      />
+      <ModeToggle />
     </div>
   )
 }
